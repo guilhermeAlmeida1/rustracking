a03be4c153dff3e3f0e2548d018171fb58a7fc5a
Guilherme Almeida <guia4273@hotmail.com>
2024-08-15 15:33:10 +0100

    Main plot 3d cartesian

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let root = BitMapBackend::new(OUT_FILE_NAME, (1024, 768)).into_drawing_area();
     root.fill(&WHITE)?;
+
     let mut chart = ChartBuilder::on(&root)
-        .caption("caption", ("sans-serif", 50))
-        .build_cartesian_2d(
+        .caption("boxDetector", ("sans-serif", 40))
+        .build_cartesian_3d(
+            -plot_dim / 2.0..plot_dim / 2.0,
             -plot_dim / 2.0..plot_dim / 2.0,
             -plot_dim / 2.0..plot_dim / 2.0,
         )?;
@@ -26,20 +28,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let projection: Vec<_> = module
             .vertices()
             .iter()
-            .map(|&it| (it.data[0], it.data[1]))
+            .map(|&it| (it.data[0], it.data[1], it.data[2]))
             .collect();
-        // let projection: Vec<_> = module
-        //     .vertices()
-        //     .iter()
-        //     .map(|&it| (it.data[1], it.data[2]))
-        //     .collect();
-        // println!("{:?}", module.vertices());
 
         chart.draw_series(std::iter::once(PathElement::new(projection, RED)))?;
     }
 
     // To avoid the IO failure being ignored silently, we manually call the present function
     root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
+    println!("Saved output to file: {}", OUT_FILE_NAME);
 
     Ok(())
 }
